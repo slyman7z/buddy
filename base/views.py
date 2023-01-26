1f05d81bcc09f0fb7387d4b29ace6b1fa34d192d
suleiman <isuleiman37@gmail.com>
2023-01-26 10:39:31 +0100

    done routing

--- base/views.py
+++ base/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 
 
 # Create your views here.
-rooms = [
+'''rooms = [
     {'id':1, 'name':'Lets learn Python'},
     {'id':2, 'name': 'Design with me'},
     {'id':3, 'name': 'frontend developers'},
-]
+] '''
 
 def home(request):
-
-    context = {'rooms':rooms}
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        context = {'room': room}
+    room = Room.objects.get(id = pk)
+    context = {'room':room}
     return render(request,'base/room.html', context)
 
 
